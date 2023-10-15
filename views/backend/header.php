815af4e0d031dddf5d7b4c2fa6789c6763c01bda
NguyenManhHung <nguyenmanhhung176472@gmail.com>
2023-10-15 08:31:57 +0700

    buoi05

--- views/backend/header.php
+++ views/backend/header.php
@@ -66,7 +66,7 @@
          <div class="sidebar">
 <div class="user-panel mt-3 pb-3 mb-3 d-flex">
                <div class="image">
-                  <img src="../public/dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+                  <img src="../public/dist/img/anhđ.jpg" class="img-circle elevation-2" alt="User Image">
                </div>
                <div class="info">
                   <a href="#" class="d-block">Nguyễn Mạnh Hùng</a>
